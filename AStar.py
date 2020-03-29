285ab933675ef4550447f0d17f2d6c71c87a84c0
Atomiccloud <Atomicans+github@gmail.com>
2020-03-29 16:30:13 -0400

    AI Project - Implementing A*

---
+++ AStar.py
@@ -0,0 +1,134 @@
+from math import sqrt
+from Node import Node
+import heapq as h
+from DisplayGUI import GUI
+import time
+
+
+def onSeg(p, q, r):
+    if (min(p[0], r[0]) >= q[0] > max(p[0], r[0])) and (min(p[1], r[1]) >= q[1] > max(p[1], r[1])):
+        return True
+    return False
+
+
+def ccw(p, q, r):
+    val = ((q[1] - p[1]) * (r[0] - q[0])) - ((q[0] - p[0]) * (r[1] - q[1]))
+    if val == 0:
+        return val
+    return 1 if val > 0 else 2
+
+
+def intersect(p1, p2, p3, p4):
+    ccw1, ccw2, ccw3, ccw4 = ccw(p1, p2, p3), ccw(p1, p2, p4), ccw(p3, p4, p1), ccw(p3, p4, p2)
+
+    if ccw1 != ccw2 and ccw3 != ccw4:
+        return True
+
+    if ccw1 == 0 and onSeg(p1, p3, p2):
+        return True
+    if ccw2 == 0 and onSeg(p1, p4, p2):
+        return True
+    if ccw3 == 0 and onSeg(p3, p1, p4):
+        return True
+    if ccw4 == 0 and onSeg(p3, p2, p4):
+        return True
+
+    return False
+
+
+def getChildren(node, v, e, shape):
+    children = []
+    for point in v:
+        intersected = False
+        for edge in e:
+            # check if intersects
+            if intersect(node.position, point, edge[0], edge[1]) and point not in edge and node.position not in edge:
+                intersected = True
+        if not intersected and node.position != point:
+            # check if point is in shape and adjacent
+            dissects_shape = False
+            for shpe in shape:
+                if point in shpe and node.position in shpe:
+                    if point == shpe[-1]:
+                        if not(node.position == shpe[-2] or node.position == shpe[0]) and point not in children:
+                            dissects_shape = True
+                    elif not(node.position == shpe[shpe.index(point)+1] or node.position == shpe[shpe.index(point)-1]):
+                        dissects_shape = True
+            if point not in children and not dissects_shape:
+                children.append(point)
+    return children
+
+
+def ED(point, end):
+    return sqrt(((point[0] - end[0]) ** 2) + ((point[1] - end[1]) ** 2))
+
+
+class AStar:
+    def __init__(self, v, e, shape, constraint):
+        self.v = v
+        self.e = e
+        self.shape = shape
+        self.constraint = constraint
+
+    def astar(self, start, end, user_input):
+        # Create start and end node
+        start_node = Node(None, start)
+        start_node.g = start_node.h = start_node.f = 0
+        end_node = Node(None, end)
+        end_node.g = end_node.h = end_node.f = 0
+
+        # Initialize both open and closed list
+        open_list, closed_list = [], []
+
+        # Add the start node
+        h.heappush(open_list, (start_node.f, start_node))
+
+        # Loop until you find the end
+        while len(open_list) > 0:
+            # Get the current node
+            current_node = h.heappop(open_list)[1]
+            closed_list.append(current_node)
+
+            # found the end
+            if current_node == end_node:
+                path = []
+                current = current_node
+                while current is not None:
+                    path.append(current.position)
+                    current = current.parent
+                return path[::-1]
+
+            # gets points that we can go to
+            children = getChildren(current_node, self.v, self.e, self.shape)
+
+            # add children nodes to open list
+            for point in children:
+                child_node = Node(current_node, point)
+
+                # calculate scores
+                child_node.g = ED(current_node.position, point) + current_node.g
+                child_node.h = ED(point, end_node.position)
+                child_node.f = child_node.g + child_node.h
+
+                # check if node in open or closed list
+                for node in open_list:
+                    if child_node == node[1] and child_node.g > node[1].g:
+                        path = []
+                        current = current_node
+                        while current is not None:
+                            path.append(current.position)
+                            current = current.parent
+
+                        if len(path) > 1:
+                            GUI(path[::-1], self.v, False, user_input)
+                        continue
+
+                for node in closed_list:
+                    if child_node == node:
+                        continue
+
+                if child_node.f > self.constraint:
+                    continue
+
+                h.heappush(open_list, (child_node.f, child_node))
+
---
+++ DisplayGUI.py
@@ -0,0 +1,88 @@
+import time
+import pygame
+
+
+class GUI:
+    def __init__(self, path, vertex, permDisplay, u_input):
+        self.path = path
+        self.vertex = vertex
+
+        pygame.init()
+
+        # colors
+        white = (255, 255, 255)
+        black = (0, 0, 0)
+        red = (255, 0, 0)
+        green = (75, 255, 50)
+
+        gameDisplay = pygame.display.set_mode((900, 540))
+        pygame.display.set_caption('Smarter than turtle')
+        gameDisplay.fill(white)
+
+        size = pygame.display.get_surface().get_size()
+        gridXSize = size[0] // 30
+        gridYSize = size[1] // 18
+
+        x = [gridXSize * x for x in range(30)]
+        y = [gridYSize * y for y in range(18)]
+        v = []
+        for point in vertex:
+            x = point[0] * gridXSize
+            y = point[1] * gridYSize
+            v.append((x, y))
+
+        if u_input == '1':
+            pygame.draw.polygon(gameDisplay, black, (v[0], v[1], v[2], v[3]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[4], v[5], v[6], v[7], v[8]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[9], v[10], v[11]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[12], v[13], v[14], v[15]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[16], v[17], v[18], v[19]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[20], v[21], v[22], v[23]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[24], v[25], v[26]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[27], v[28], v[29], v[30], v[31], v[32]), 4)
+
+            # start dot and font
+            pygame.draw.circle(gameDisplay, green, (30, 420), 5)
+            font = pygame.font.SysFont('Arial', 20, True)
+            startText = font.render('Start', True, black)
+            gameDisplay.blit(startText, (10, 420))
+
+            # end
+            pygame.draw.circle(gameDisplay, red, (870, 31), 5)
+            startText = font.render('End', True, black)
+            gameDisplay.blit(startText, (865, 5))
+        else:
+            pygame.draw.polygon(gameDisplay, black, (v[0], v[1], v[2], v[3], v[4]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[5], v[6], v[7], v[8]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[9], v[10], v[11], v[12]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[13], v[14], v[15], v[16], v[17]), 4)
+            pygame.draw.polygon(gameDisplay, black, (v[18], v[19], v[20]), 4)
+
+            # start dot and font
+            pygame.draw.circle(gameDisplay, green, (60, 150), 5)
+            font = pygame.font.SysFont('Arial', 20, True)
+            startText = font.render('Start', True, black)
+            gameDisplay.blit(startText, (50, 140))
+
+            # end
+            pygame.draw.circle(gameDisplay, red, (750, 330), 5)
+            startText = font.render('End', True, black)
+            gameDisplay.blit(startText, (745, 330))
+
+
+
+        #path
+        p = []
+        for point in path:
+            x = point[0] * gridXSize
+            y = point[1] * gridYSize
+            p.append((x, y))
+
+        pygame.draw.lines(gameDisplay, red, False, p, 4)
+
+        pygame.display.update()
+
+        if permDisplay:
+            pygame.display.update()
+            time.sleep(7)
+            pygame.display.quit()
